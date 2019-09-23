3a0178d18e41f1fe5a93e22d27b3edd7e5ec40d9
Priyanshu Nayan <priyanshunayan456@gmail.com>
2019-09-23 20:40:56 +0530

    hello world react

---
+++ parth-web/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom'
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <p>Hello World!</p>
+      </div>
+    )
+  }
+}
+
+export default App;
