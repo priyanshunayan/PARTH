f0b3c0519f63a54612fba2a25734121cf5d09b95
Priyanshu Nayan <priyanshunayan456@gmail.com>
2019-09-24 19:59:10 +0530

    story component ready

--- parth-web/src/components/about/about.js
+++ parth-web/src/components/about/about.js
@@ -2,36 +2,42 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import './about.css'
+import styles from './about.module.css'
+
+
 export class About extends Component {
     render(){
+        console.log("STYLES", styles);
         return (
-            <div className="about">
-                    <Row class="row">
-                        <Col lg={true} className="left-column">
-                            <div className="text-container">
-                            <h3 className="space">We are serving to more than 10 million people & next milestone is 1 billion</h3>
-                            <p className="gray"> Want to be part of this mission?</p>
-                            <button className="join">Join us</button>
+            <div className={styles.about}>
+                <Container fluid={true}>
+                    <Row  noGutters={true}>
+                        <Col  className={styles.leftColumn}>
+                            <div className={styles.textContainer}>
+                            <h3 className={styles.space}>We are serving to more than 10 million people & next milestone is 1 billion</h3>
+                            <p className={styles.gray}> Want to be part of this mission?</p>
+                            <button className={styles.join}>Join us</button>
                             </div>
                         </Col>
-                        <Col lg={true} className="right-column">
+                        <Col className={styles.rightColumn}>
                         </Col>
                     </Row>
-                    <div className="achievements">
-                    <div className="rectangle">
+                    </Container>
+                    <div className={styles.achievements}>
+                    <div className={styles.rectangle}>
                         <div>Google Play 10 million club</div>
                     </div>
-                    <div className="rectangle">
+                    <div className={styles.rectangle}>
                         <div>Title Sponsor of NITP Fest</div>
                     </div>
-                    <div className="rectangle">
+                    <div className={styles.rectangle}>
                         <div>Invited in Google App Summit</div>
                     </div>
-                    <div className="rectangle">
+                    <div className={styles.rectangle}>
                         <div>Featured in International media</div>
                     </div>
                     </div>
+                    
             </div>
         )
     }
--- parth-web/src/components/story/story.js
+++ parth-web/src/components/story/story.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
-
-
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import styles from './story.module.css'
 export class Story extends Component {
     render(){
         return(
-            <div>
-                <h3>This is my Story</h3>
+            <div className={styles.story}>
+                <Container fluid={true}>
+                <Row noGutters={true}>
+                <Col className={styles.leftColumn}>
+                </Col>
+                <Col className={styles.rightColumn}>
+                    <div className={styles.text}>
+                        <h4 className={styles.heading}>Our Story</h4>
+                        <p className={styles.description}>Parth Universal is a product based organisation, have multiple applications in sports and social platform category. Our user base is over 10 million covering 50+ countries across the world. We focus on extensive designs and great user experience. Parth Universal is in the market since 3 years with an exponential growth rate. Parth Universal make quality of people’s life better by providing services and opportunities. The team is young and highly enthusiastic who are competing with the tech tycoons out there.</p>
+                    </div>
+                </Col>
+                </Row>
+                <div>
+                    <div className={styles.boxes}>
+                        <div className={styles.blackBox1}>
+                            <div>
+                                <span className={styles.stat}>50+</span><br/>Countries
+                            </div>
+                        </div>
+                            <div className={styles.blackBox2}>
+                                <div>
+                                    <span className={styles.stat}>50+</span><br/>Countries
+                                </div>
+                            </div>
+                    </div>
+                    <div className={styles.boxes2}>
+                        <div className={styles.blackBox3}>
+                            <div >
+                                <span className={styles.stat}>50+</span><br/>Countries
+                            </div>
+                        </div>
+                            <div className={styles.blackBox4}>
+                                <div>
+                                    <span className={styles.stat}>50+</span><br/>Countries
+                                </div>
+                            </div>
+                    </div>  
+                </div>
+                </Container>
             </div>
         )
     }
